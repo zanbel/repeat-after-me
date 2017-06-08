b214fa84cdfa5fb4cf53204e748ef5ec3f2ceb05
zanbel <royz@jfrog.com>
2017-06-09 00:14:51 +0300

    fixed main method

--- app.js
+++ app.js
@@ -1,46 +1,30 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+/**
+ * Created by royz on 08/06/2017.
+ */
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-var app = express();
+const app= express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.post('/repeat-after-me', function(req, res) {
+    var speech = req.body.result && req.body.result.parameters &&
+                req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Master... I couldn't understand... please speak to me."
+    return res.json({
+        speech: speech,
+        displayText: speech,
+        source: 'repeat-after-me'
+    });
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.get('/', function (req, res) {
+    res.send('This is my test repeat after me app.')
+})
 
 module.exports = app;
--- index.js
+++ index.js
@@ -1,20 +1,19 @@
 /**
  * Created by royz on 08/06/2017.
  */
-'use strict';
 
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const restService = express();
+const app= express();
 
-restService.use(bodyParser.urlencoded({
-    extended: true
-}));
+// restService.use(bodyParser.urlencoded({
+//     extended: true
+// }));
+//
+// restService.use(bodyParser.json());
 
-restService.use(bodyParser.json());
-
-restService.post('/repeat-after-me', function(req, res) {
+app.post('/repeat-after-me', function(req, res) {
     var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
     return res.json({
         speech: speech,
@@ -23,6 +22,14 @@ restService.post('/repeat-after-me', function(req, res) {
     });
 });
 
-restService.listen((process.env.PORT || 8000), function() {
-    console.log("Server up and listening");
+app.get('/', function (req, res) {
+    res.send('Hello World')
+})
+
+// restService.listen((process.env.PORT || 8000), function() {
+//     console.log("Server up and listening");
+// });
+
+app.listen((process.env.PORT || 3000), function () {
+    console.log('Example app listening on port 3000!')
 });
